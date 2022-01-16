eca8f1672c173f48a8cc9487672bf981bdb94bc2
Pratyush Kumar <pratyush20454@iiitd.ac.in>
2022-01-17 04:38:04 +0530

    Instrumentation test passed
    Signed-off-by: Pratyush Kumar <pratyush20454@iiitd.ac.in>

--- app/src/androidTest/java/ads/tipcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ads/tipcalculator/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package ads.tipcalculator
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,17 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ads.tipcalculator", appContext.packageName)
+    fun calculate_default_tip() {
+        Espresso.onView(ViewMatchers.withId(R.id.cost_of_service_edit_text))
+            .perform(ViewActions.typeText("50.00"))
+
+        Espresso.onView(ViewMatchers.withId(R.id.calculate_button)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tip_result))
+            .check(ViewAssertions.matches(ViewMatchers.withText(Matchers.containsString("10.00"))))
     }
 }
